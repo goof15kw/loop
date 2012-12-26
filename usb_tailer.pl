4b577d988f40cdf92d0057130d32564035ffaa71
Goof <lmgelinas@gmail.com>
2012-12-26 12:27:42 -0500

    ajout note + exit au bon endroit

--- usb_tailer.pl
+++ usb_tailer.pl
@@ -7,8 +7,9 @@ while (1) {
 	if ( ! $port ) {
 		print "Marche pas, next\n";
 		$port = Device::SerialPort->new("/dev/ttyACM1");
-		if (! $port ) { print "Cannot open 0 nor 1 , ABORT\n"; }
+		if (! $port ) { print "Cannot open 0 nor 1 , ABORT\n"; exit;}
 		else { print "Opened ACM1"; }
+
 	} else { print "Opened ACM0"; }
 	$port->databits(8);
 	$port->baudrate(9600); # <-- match to arduino settings
